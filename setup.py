5113ac3aad2253ca2a840c80f62ddec66d98b10c
Shrikanth Mahale <shrikanth.mahale@gmail.com>
2024-08-06 13:57:48 -0400

    Installed Packages and Created Folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Source Code Analysis',
+    version= '0.0.0',
+    author= 'Lord Shrikanth',
+    author_email= 'shrikanth.mahale@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
